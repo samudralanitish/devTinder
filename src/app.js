199135a5765eb9abc4d4c31ca598986116921262
nitis0315 <nitishsamudrala2002@gmail.com>
2024-10-12 21:26:59 +0530

    exporing get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,30 @@ const express=require("express");
 
 const app=express();
 
+app.get("/hello",(req,res)=>{
+    res.send("I am hello wala GET")
+})
+
+app.post("/hello",(req,res)=>{
+    res.send("I am hello wala POST")
+})
+
+app.use("/hello/ab",(req,res)=>{
+    res.send("abrakadabra")
+}) 
+
+
+app.use("/hello",(req,res)=>{
+    res.send("hello hello hello")
+})
+
 app.use("/test",(req,res)=>{
     res.send("test from server")
 })
 app.use("/",(req,res)=>{
     res.send("enpty server")
-})
+}) 
+
 app.listen(3000,()=>{
     console.log("Server is sucessfully running on port 3000");
     
